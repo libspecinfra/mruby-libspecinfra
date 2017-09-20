1a6b82996d4430f1f2a6cbd59b9f922153f167dd
Gosuke Miyashita <gosukenator@gmail.com>
2017-09-20 16:32:02 +0900

    file.group() raised exception when the file does not exist

--- test/file.rb
+++ test/file.rb
@@ -57,4 +57,5 @@ assert("file does not exist") do
   assert_raise(RuntimeError, "entity not found") { f.is_symlink() }
   assert_raise(RuntimeError, "entity not found") { f.contents() }
   assert_raise(RuntimeError, "entity not found") { f.owner() }
+  assert_raise(RuntimeError, "entity not found") { f.group() }
 end
