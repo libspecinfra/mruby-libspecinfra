43250ec8b56a58eaa1cb5ae672a5a3ba2a7e1206
Gosuke Miyashita <gosukenator@gmail.com>
2017-10-23 02:00:30 +0900

    Remove gcc warnings

--- src/backend_ssh.c
+++ src/backend_ssh.c
@@ -4,15 +4,18 @@
 static mrb_value backend_ssh_initialize(mrb_state *mrb, mrb_value self) {
     char *str;
     mrb_int len;
-    mrb_get_args(mrb, "s", &str, &len);
+    backend_t *b;
 
-    backend_t *b = backend_ssh_new(str);
+    mrb_get_args(mrb, "s", &str, &len);
+    b = backend_ssh_new(str);
     DATA_TYPE(self) = &mrb_backend_type;
     DATA_PTR(self) = b;
     return self;
 }
 
 void backend_ssh_init(mrb_state *mrb, struct RClass *b) {
-    struct RClass *d = mrb_define_class_under(mrb, b, "SSH", mrb->object_class);
+    struct RClass *d;
+
+    d = mrb_define_class_under(mrb, b, "SSH", mrb->object_class);
     mrb_define_method(mrb, d, "initialize", backend_ssh_initialize, MRB_ARGS_REQ(1));
 }
