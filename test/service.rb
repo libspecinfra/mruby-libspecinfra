2cbc61ac35d22b28426efc12ad93a5ab237e5951
Gosuke Miyashita <gosukenator@gmail.com>
2017-10-23 20:42:28 +0900

    Add tests of other services

--- test/service.rb
+++ test/service.rb
@@ -2,9 +2,14 @@ b = Libspecinfra::Backend::Direct.new()
 s = Libspecinfra::Specinfra.new(b)
 
 assert("nginx") do
+  dbus = s.service("dbus")
+  sshd = s.service("sshd")
   nginx = s.service("nginx")
 
   if ENV['TEST_SERVICE']
+    assert_true(dbus.is_running())
+    assert_true(sshd.is_enabled())
+
     assert_true(nginx.is_running())
     assert_true(nginx.is_enabled())
   end
