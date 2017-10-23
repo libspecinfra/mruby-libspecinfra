c1a770103644f0676f0041ad73b45ef152c16686
Gosuke Miyashita <gosukenator@gmail.com>
2017-10-23 16:03:36 +0900

    Implement service.error_description() and service.is_running()

--- src/resource_service.c
+++ src/resource_service.c
@@ -3,7 +3,37 @@
 #include "mruby/string.h"
 #include "resource_service.h"
 
+mrb_value resource_service_error_description_(mrb_state *mrb, mrb_value self) {
+    mrb_value str;
+    char *c;
+
+    c = resource_service_error_description(DATA_PTR(self));
+    str = mrb_str_buf_new(mrb, sizeof(c));
+    return mrb_str_cat2(mrb, str, c);
+}
+
+mrb_value resource_service_is_running_(mrb_state *mrb, mrb_value self) {
+    resource_service_t *f;
+    int32_t m;
+
+    f = DATA_PTR(self);
+    m = resource_service_is_running(f);
+
+    if ( m == 1 ) {
+        return mrb_true_value();
+    } else if ( m == 0 ) {
+        return mrb_false_value();
+    } else {
+        mrb_raise(
+            mrb,
+            E_RUNTIME_ERROR,
+            resource_service_error_description(f)
+        );
+    }
+}
+
 void resource_service_init(mrb_state *mrb, struct RClass *r) {
     struct RClass *f = mrb_define_class_under(mrb, r, "Service", mrb->object_class);
-
+    mrb_define_method(mrb, f, "error_description", resource_service_error_description_, MRB_ARGS_NONE());
+    mrb_define_method(mrb, f, "is_running", resource_service_is_running_, MRB_ARGS_NONE());
 }
--- src/resource_service.h
+++ src/resource_service.h
@@ -5,3 +5,9 @@ typedef struct resource_service_S resource_service_t;
 const static struct mrb_data_type mrb_resource_service_type = { "Service", mrb_free };
 
 void resource_service_init(mrb_state *mrb, struct RClass *r);
+
+extern char *resource_service_error_description(resource_service_t *f);
+mrb_value resource_service_error_description_(mrb_state *mrb, mrb_value self);
+
+extern int32_t resource_service_is_running(resource_service_t *f);
+mrb_value resource_service_is_running_(mrb_state *mrb, mrb_value self);
